61c5db40d33985775942011cf10b69ea409299d8
shobha-HK <153706602+shobha-HK@users.noreply.github.com>
2023-12-14 16:08:20 +0530

    Update shobha.java

--- shobha.java
+++ shobha.java
@@ -2,7 +2,7 @@ public class sample {
 
     public static void main("String" args[])
     {
-        System.out.println("shobha and komala and ranjita");
+        System.out.println("shobha and komala and ranjita and nishchita");
     }
     
 }
